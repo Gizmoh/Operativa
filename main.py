61cd950c1609d9e68aa6a2df20a4be84b18bbd8e
Dalanth <niico.ex@gmail.com>
2014-12-02 17:38:12 -0300

    Faltaba interfaz

---
+++ main.py
@@ -0,0 +1,121 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys, os
+from main_ui import Ui_MainWindow
+from PySide import QtCore, QtGui
+import random
+import math
+
+class MainWindow(QtGui.QMainWindow):
+
+	def __init__(self):
+		super(MainWindow, self).__init__()
+		self.ui = Ui_MainWindow()
+		self.ui.setupUi(self)
+		self.set_listeners()
+	
+		self.shift_selector = 0 #determina que turno se ejecutara a continuacion.
+		self.shift_worked01 = 0 #Cantidad de turnos trabajados por maquina 1
+		self.shift_worked02 = 0 #Cantidad de turnos trabajados por maquina 2
+
+		self.TruckLoad = 0 #Carga sobrante entre turnos.
+		self.proLoad = 0 #Carga al principio del turno
+		self.Package01 = 0 #Cantidad de paquetes hechos por la primera maquina
+		self.Package02 = 0 #Cantidad de paquetes hechos por la segunda maquina
+		self.Packages = 0 #Cantidad de paquetes por turno.
+
+		self.Time_Off01 = 0 #Cantidad de tiempo que la maquina permanece sin uso
+		self.Time_Off02 = 0 #Cantidad de tiempo que la maquina permanece sin uso
+		self.Time_On01 = 0 #Cantidad de tiempo que la maquina se encuentra ocupada
+		self.Time_On02 = 0 #Cantidad de tiempo que la maquina se encuentra ocupada
+
+	def about(self):
+		"""
+		Ventana con los integrantes del grupo
+		"""
+		message = u'Integrantes: \n- Nicolas Aravena\n- Hernan Casanova\n- Arturo Reyes\n\n'
+		message += u'Profesor responsable: \n - Miguelina Vega Rosales\n\n'
+		message += u'Simulador realizado para el ramo de Investigación Operativa II\n'
+		self.ui.about = QtGui.QMessageBox.information(self, 'Acerca de Simulador', message)
+
+
+	def set_listeners(self):
+		"""
+		Sets up button listeners
+		"""
+		self.ui.aboutUs.triggered.connect(self.about)
+		self.ui.simulate.clicked.connect(self.simulate)
+
+
+	def CreaCarga(self, TruckLoad):#Creamos los camiones que llegan ese turno
+		Time = 0 #Inicializamos la variable en 0
+		Trucks_shift = 0
+		while Time < 480: #agregamos mas camiones al turno hasta que se completen los 480 minutos
+			Time = Time + random.expovariate(0.05) #agregamos una cantidad de tiempo al azar a los minutos para simular la llegada de un camion
+			Trucks_shift = Trucks_shift + 1 #agregamos un contador al contador de camiones
+		Trucks_shift = math.ceil(Trucks_shift)
+		Trucks_shift = Trucks_shift + self.TruckLoad #agregamos los camiones sobrantes del turno anterior a los camiones para este turno
+		return Trucks_shift
+
+
+	def Machine01(self, carga):#Turno de la maquina 1
+		global Package01
+		global shift_worked01
+		global Time_Off01
+		time = 480
+		while time > 0 and carga > 0:#Maquina opera mientras dure el turno o duren las cargas de los camiones, lo que se acabe primero
+			time = time - random.normalvariate(12,5)
+			carga = carga-1
+			self.Package01 = self.Package01 + 1
+		if time > 0: #Si el turno se termina por falta de cargas se suma el tiempo sobrante al tiempo desocupado
+			self.Time_Off01 = self.Time_Off01 + time
+			self.Time_On01 = 480 - time
+		self.shift_worked01 = self.shift_worked01 + 1
+		return carga
+
+
+	def Machine02(self, carga):#Turno de la maquina 2
+		global Package02
+		global shift_worked02
+		global Time_Off02
+		time = 480
+		while time > 0 and carga > 0:#Maquina opera mientras dure el turno o duren las cargas de los camiones, lo que se acabe primero
+			time = time - random.uniform(12,15)
+			carga = carga - 2
+			self.Package02 = self.Package02 + 1
+		if time > 0: #Si el turno se termina por falta de cargas se suma el tiempo sobrante al tiempo desocupado
+			self.Time_Off01 = time
+			self.Time_On01 = 480 - time
+		self.shift_worked02 = self.shift_worked02 + 1
+		return carga
+
+	def simulate(self):
+		shift_count = 10 #En esta variable se guarda la cantidad de turnos a contar (" aca va el input de los turnos")
+		for i in range(0,shift_count): # el proceso funciona hasta pasar por todos los turnos introducidos
+			if self.shift_selector == 0: #Turno de la primera maquina
+				self.shift_selector = 1
+				self.proLoad = self.CreaCarga(self.TruckLoad)
+				self.TruckLoad = self.Machine01(self.proLoad)
+			if self.shift_selector == 1: #Turno de la segunda maquina
+				self.shift_selector = 2
+				self.proLoad = self.CreaCarga(self.TruckLoad)
+				self.TruckLoad = self.Machine02(self.proLoad)
+			if self.shift_selector == 2: #Turno de descanso, no se hace nada, no llegan camiones.
+				self.shift_selector = 0
+		self.Packages = self.Package01 + self.Package02
+		print ("Carga sobrante final = "+str(self.TruckLoad)+" cargas ; paquetes totales creados = "+str(self.Packages)+".")
+		print ("Tiempo desocupado promedio maquina 1 = "+str(self.Time_Off01/self.shift_worked01)+" minutos.")
+		print ("Tiempo desocupado promedio maquina 2 = "+str(self.Time_Off02/self.shift_worked02)+" minutos.")
+		print ("Paquetes creados maquina 2 = "+str(self.Package02)+".")
+
+
+def main():
+	app = QtGui.QApplication(sys.argv)
+	main = MainWindow()
+	main.show()
+	sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+	main()
---
+++ main_ui.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'main_ui.ui'
+#
+# Created: Tue Dec  2 17:36:37 2014
+#      by: pyside-uic 0.2.15 running on PySide 1.2.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PySide import QtCore, QtGui
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(364, 386)
+        MainWindow.setMinimumSize(QtCore.QSize(364, 386))
+        MainWindow.setMaximumSize(QtCore.QSize(364, 386))
+        palette = QtGui.QPalette()
+        brush = QtGui.QBrush(QtGui.QColor(0, 174, 255))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.Highlight, brush)
+        brush = QtGui.QBrush(QtGui.QColor(0, 174, 255))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.Highlight, brush)
+        brush = QtGui.QBrush(QtGui.QColor(174, 174, 174))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.Highlight, brush)
+        MainWindow.setPalette(palette)
+        MainWindow.setMouseTracking(True)
+        MainWindow.setLocale(QtCore.QLocale(QtCore.QLocale.Spanish, QtCore.QLocale.Chile))
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayoutWidget = QtGui.QWidget(self.centralwidget)
+        self.gridLayoutWidget.setGeometry(QtCore.QRect(10, 10, 341, 321))
+        self.gridLayoutWidget.setObjectName("gridLayoutWidget")
+        self.gridLayout = QtGui.QGridLayout(self.gridLayoutWidget)
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout.setObjectName("gridLayout")
+        self.simulate = QtGui.QPushButton(self.gridLayoutWidget)
+        self.simulate.setObjectName("simulate")
+        self.gridLayout.addWidget(self.simulate, 5, 0, 1, 1)
+        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.label = QtGui.QLabel(self.gridLayoutWidget)
+        self.label.setEnabled(True)
+        self.label.setObjectName("label")
+        self.horizontalLayout.addWidget(self.label)
+        self.turns = QtGui.QSpinBox(self.gridLayoutWidget)
+        self.turns.setMaximum(16777215)
+        self.turns.setObjectName("turns")
+        self.horizontalLayout.addWidget(self.turns)
+        self.gridLayout.addLayout(self.horizontalLayout, 2, 0, 1, 1)
+        self.label_2 = QtGui.QLabel(self.gridLayoutWidget)
+        self.label_2.setObjectName("label_2")
+        self.gridLayout.addWidget(self.label_2, 3, 0, 1, 1)
+        self.results = QtGui.QTextBrowser(self.gridLayoutWidget)
+        self.results.setObjectName("results")
+        self.gridLayout.addWidget(self.results, 4, 0, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtGui.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 364, 25))
+        self.menubar.setObjectName("menubar")
+        self.help = QtGui.QMenu(self.menubar)
+        self.help.setObjectName("help")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.aboutUs = QtGui.QAction(MainWindow)
+        self.aboutUs.setObjectName("aboutUs")
+        self.help.addAction(self.aboutUs)
+        self.menubar.addAction(self.help.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "Simulador", None, QtGui.QApplication.UnicodeUTF8))
+        self.simulate.setText(QtGui.QApplication.translate("MainWindow", "Simular", None, QtGui.QApplication.UnicodeUTF8))
+        self.label.setText(QtGui.QApplication.translate("MainWindow", "Turnos a contar", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_2.setText(QtGui.QApplication.translate("MainWindow", "Resultados de la Simulación", None, QtGui.QApplication.UnicodeUTF8))
+        self.help.setTitle(QtGui.QApplication.translate("MainWindow", "Ayuda", None, QtGui.QApplication.UnicodeUTF8))
+        self.aboutUs.setText(QtGui.QApplication.translate("MainWindow", "Acerca de", None, QtGui.QApplication.UnicodeUTF8))
+
